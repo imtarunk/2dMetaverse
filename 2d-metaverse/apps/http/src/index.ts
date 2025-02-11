3d047de1288d26c06816e3cda19f88c9aa406bef
imtarunk <tarunshr145@gmail.com>
2025-02-11 15:49:09 +0530

    authentication route complete

--- 2d-metaverse/apps/http/src/index.ts
+++ 2d-metaverse/apps/http/src/index.ts
@@ -1,12 +1,14 @@
+import { configDotenv } from "dotenv";
 import express from "express";
-import { router } from "./routes/v1"; //@ts-ignore
-import client from "@repo/db";
+import { router } from "./routes/v1";
 
+configDotenv();
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
 app.use("/api/v1", router);
 
-app.listen(PORT, () => {
-  console.log("server is running " + PORT);
+const port = process.env.PORT || 2000;
+app.listen(port, () => {
+  console.log("server is running " + port);
 });
--- 2d-metaverse/apps/http/src/routes/v1/index.ts
+++ 2d-metaverse/apps/http/src/routes/v1/index.ts
@@ -2,17 +2,84 @@ import { Router } from "express";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
 import { userRouter } from "./user";
+import { SigninSchema, SignupSchema } from "../../types";
+import client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { configDotenv } from "dotenv";
+import bcrypt from "bcrypt";
 
+configDotenv();
 export const router = Router();
 
-router.post("/login", (req, res) => {
-  return console.log("routing is success");
+router.post("/signin", async (req, res) => {
+  const parsed = SigninSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    res
+      .status(400)
+      .json({ message: "Entre a valid input and try to login in again" });
+  }
+
+  try {
+    const user = await client.user.findUnique({
+      where: {
+        username: parsed.data?.username,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+      return;
+    }
+
+    const isValid = await bcrypt.compare(
+      parsed.data?.password as string,
+      user.password
+    ); // true
+
+    if (isValid) {
+      const token = jwt.sign(user.id, "key");
+      res.status(200).json({
+        message: "user logged in success",
+        success: true,
+        token: token,
+      });
+      return;
+    }
+  } catch (e) {
+    res.status(400).json({ message: "internal server error" });
+  }
 });
 
-router.post("/signup", (req, res) => {
-  res.status(200).json({
-    message: "routting success",
-  });
+router.post("/signup", async (req, res) => {
+  const parsed = SignupSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    res.status(400).json({ message: "validation error" });
+    return;
+  }
+
+  const hashPassword = bcrypt.hashSync(parsed.data.password, 5);
+
+  try {
+    const newUser = await client.user.create({
+      data: {
+        username: parsed.data.username,
+        password: hashPassword,
+        role: parsed.data.type === "admin" ? "Admin" : "User",
+      },
+    });
+
+    res.status(200).json({
+      message: "user signup success",
+      success: true,
+      userId: newUser.id,
+    });
+    return;
+  } catch (err) {
+    res.status(400).json({ message: "User already exists" });
+    console.log("error:" + err);
+  }
 });
 
 router.get("/avaters", (req, res) => {});
